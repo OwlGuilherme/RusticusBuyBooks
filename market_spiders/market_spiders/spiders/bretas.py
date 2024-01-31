e350828e43cad9281ccc8e760aad40fc0b5b0df7
Guilherme Santos <guilhermesantos.adv@protonmail.com>
2024-01-30 21:33:45 -0300

    Implementadas funcionalidades que impedem a criação duplicada de produtos

--- market_spiders/market_spiders/spiders/bretas.py
+++ market_spiders/market_spiders/spiders/bretas.py
@@ -1,11 +1,39 @@
-from typing import Any, Iterable
 import scrapy
 import json
-
+import sqlite3
+from scrapy.exceptions import CloseSpider
 
 class BretasSpider(scrapy.Spider):
     name = "bretas"
     allowed_domains = ["bretas.com.br"]
+
+    def __init__(self):
+        super().__init__()
+        self.setup_database()
+
+    def setup_database(self):
+        try:
+            conn = sqlite3.connect('bretas.db')
+            cursor = conn.cursor()
+
+            # Criação da tabela de categorias
+            cursor.execute('''CREATE TABLE IF NOT EXISTS categorias (
+                                id_categoria INTEGER PRIMARY KEY,
+                                nome_categoria TEXT UNIQUE)''')
+
+            # Criação da tabela de produtos
+            cursor.execute('''CREATE TABLE IF NOT EXISTS produtos (
+                                id_produto INTEGER PRIMARY KEY,
+                                nome_produto TEXT,
+                                preco TEXT,
+                                id_categoria INTEGER,
+                                FOREIGN KEY (id_categoria) REFERENCES categorias(id_categoria))''')
+
+            conn.commit()
+            conn.close()
+        except sqlite3.Error as error:
+            print("Erro ao criar o banco de dados:", error)
+            raise CloseSpider('Erro ao criar o banco de dados')
 
     def start_requests(self):
         with open("links.json") as file:
@@ -28,6 +56,9 @@ class BretasSpider(scrapy.Spider):
             preco = produto.xpath('.//h5[contains(@class, "bretas-bretas-components-0-x-crmDiscount")]/text()').get()
             lista_produtos_pagina.append({'nome': nome, 'preco': preco})
 
+            # Inserir produto no banco de dados
+            self.inserir_produto(categoria, nome, preco)
+
         # Adiciona as informações dos produtos desta página à lista de produtos da categoria
         if lista_produtos_pagina:
             yield {'categoria': categoria, 'produtos_pagina': lista_produtos_pagina}
@@ -38,15 +69,27 @@ class BretasSpider(scrapy.Spider):
             yield scrapy.Request(url=next_page_url, meta={'categoria': categoria, 'page_number': next_page_number}, callback=self.parse)
 
     def closed(self, reason):
-        # Após o término do spider, combina todas as informações coletadas em um único arquivo JSON
-        all_data = {}
-        with open("output.json", "w") as json_file:
-            for line in open("output.json"):
-                data = json.loads(line)
-                categoria = data['categoria']
-                produtos_pagina = data['produtos_pagina']
-                if categoria not in all_data:
-                    all_data[categoria] = []
-                all_data[categoria].extend(produtos_pagina)
+        pass
 
-            json.dump(all_data, json_file, ensure_ascii=False, indent=2)
+    def inserir_produto(self, categoria, nome, preco):
+        try:
+            conn = sqlite3.connect('bretas.db')
+            cursor = conn.cursor()
+
+            # Verificar se a categoria já está na tabela
+            cursor.execute('SELECT id_categoria FROM categorias WHERE nome_categoria = ?', (categoria,))
+            categoria_id = cursor.fetchone()
+            if categoria_id is None:
+                cursor.execute('INSERT INTO categorias (nome_categoria) VALUES (?)', (categoria,))
+                categoria_id = cursor.lastrowid
+            else:
+                categoria_id = categoria_id[0]
+
+            # Inserir produto na tabela de produtos
+            cursor.execute('INSERT INTO produtos (nome_produto, preco, id_categoria) VALUES (?, ?, ?)', (nome, preco, categoria_id))
+
+            conn.commit()
+            conn.close()
+        except sqlite3.Error as error:
+            print("Erro ao inserir produto:", error)
+
