dd65108db2faf973339e34dd311c164d611635aa
Guilherme Santos <guilhermesantos.adv@protonmail.com>
2024-07-04 16:44:15 -0300

    Melhora gerenciamento de conexões com o banco de dados

--- rusticus_buy_books/src/modulos/database.rs
+++ rusticus_buy_books/src/modulos/database.rs
@@ -1,78 +1,94 @@
 use std::io;
+use egui::CollapsingHeader;
 use rusqlite::{Connection, Result};
+use std::sync::{Arc, Mutex};
 
-pub fn cria_db() -> Result<()> {
-    let conn = Connection::open("compras.db")?;
-
-    conn.execute(
-        "CREATE TABLE IF NOT EXISTS produtos (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        Produto TEXT NOT NULL,
-        Marca_ou_qualidade TEXT NOT NULL,
-        Conteúdo FLOAT
-        )",
-        [],
-    )?;
-    Ok(())
+pub struct Database {
+    conn: Arc<Mutex<Connection>>,
 }
 
-pub fn add_produto() -> Result<()> {
-    let conn = Connection::open("compras.db")?;
-
-    println!("Digite o nome do produto: ");
-    let mut nome = String::new();
-    io::stdin().read_line(&mut nome).expect("Falha ao ler o nome");
-
-    println!("Digite a marca do produto:");
-    let mut marca = String::new();
-    io::stdin().read_line(&mut marca).expect("Falha ao ler a marca");
-
-    println!("Digite o conteúdo do produto [litros ou km]: ");
-    let mut conteudo = String::new();
-    io::stdin().read_line(&mut conteudo).expect("Falha ao ler conteúdo");
-
-    conn.execute(
-        "INSERT INTO produtos (Produto, Marca_ou_qualidade, Conteúdo) VALUES (?1, ?2, ?3)",
-        (&nome.trim(), &marca.trim(), &conteudo.trim())
-    )?;
-    Ok(())
-}
-
-pub fn show_produtos() -> Result<()> {
-    let conn = Connection::open("compras.db")?;
-
-    let mut stmt = conn.prepare("SELECT id, Produto, Marca_ou_qualidade, Conteúdo FROM produtos")?;
-    
-    let produtos = stmt.query_map([], |row|{
-        Ok((row.get::<_, i32>(0)?,
-            row.get::<_, String>(1)?,
-            row.get::<_, String>(2)?,
-            row.get::<_, f32>(3)?,            
-        ))
-    })?;
-
-    for produto in produtos {
-        if let Ok((id, nome, marca, conteudo)) = produto {
-            println!("ID: {}, Nome: {}, Marca: {}, Conteúdo: {}", id, nome, marca, conteudo);
-        }
+impl Database {
+    pub fn new(db_path: &str) -> Result<Self> {
+        let conn = Connection::open(db_path)?;
+        Ok(Database {
+            conn: Arc::new(Mutex::new(conn)),
+        })
     }
-    Ok(())
-}
 
-pub fn delete_produto() -> Result<()> {
-    let conn = Connection::open("compras.db")?;
+    pub fn cria_db(&self) -> Result<()> {
+        let conn = self.conn.lock().unwrap();
 
-    show_produtos()?;
+        conn.execute(
+            "CREATE TABLE IF NOT EXISTS produtos (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                Produto TEXT NOT NULL,
+                Marca_ou_qualidade TEXT NOT NULL,
+                Conteudo FLOAT,
+                Preco FLOAT
+            )",
+            [],
+        )?;
+        Ok(())
+    }
 
-    println!("Digite o ID do produto que deseja deletar: ");
-    let mut id = String::new();
-    io::stdin().read_line(&mut id).expect("Falha ao ler o ID");
+    pub fn add_produto() -> Result<()> {
+        let conn = Connection::open("compras.db")?;
 
-    // Convertendo a string lida para i32
-    let id: i32 = id.trim().parse().expect("ID inválido, digite um número inteiro");
+        println!("Digite o nome do produto: ");
+        let mut nome = String::new();
+        io::stdin().read_line(&mut nome).expect("Falha ao ler o nome");
 
-    conn.execute(
-        "DELETE FROM produtos WHERE id = ?1", &[&id]
-    )?;
-    Ok(())
+        println!("Digite a marca do produto:");
+        let mut marca = String::new();
+        io::stdin().read_line(&mut marca).expect("Falha ao ler a marca");
+
+        println!("Digite o conteúdo do produto [litros ou kg]: ");
+        let mut conteudo = String::new();
+        io::stdin().read_line(&mut conteudo).expect("Falha ao ler conteúdo");
+
+        conn.execute(
+            "INSERT INTO produtos (Produto, Marca_ou_qualidade, Conteúdo) VALUES (?1, ?2, ?3)",
+            (&nome.trim(), &marca.trim(), &conteudo.trim())
+        )?;
+        Ok(())
+    }
+
+    pub fn show_produtos() -> Result<()> {
+        let conn = Connection::open("compras.db")?;
+
+        let mut stmt = conn.prepare("SELECT id, Produto, Marca_ou_qualidade, Conteúdo FROM produtos")?;
+        
+        let produtos = stmt.query_map([], |row|{
+            Ok((row.get::<_, i32>(0)?,
+                row.get::<_, String>(1)?,
+                row.get::<_, String>(2)?,
+                row.get::<_, f32>(3)?,            
+            ))
+        })?;
+
+        for produto in produtos {
+            if let Ok((id, nome, marca, conteudo)) = produto {
+                println!("ID: {}, Nome: {}, Marca: {}, Conteúdo: {}", id, nome, marca, conteudo);
+            }
+        }
+        Ok(())
+    }
+
+    pub fn delete_produto() -> Result<()> {
+        let conn = Connection::open("compras.db")?;
+
+        show_produtos()?;
+
+        println!("Digite o ID do produto que deseja deletar: ");
+        let mut id = String::new();
+        io::stdin().read_line(&mut id).expect("Falha ao ler o ID");
+
+        // Convertendo a string lida para i32
+        let id: i32 = id.trim().parse().expect("ID inválido, digite um número inteiro");
+
+        conn.execute(
+            "DELETE FROM produtos WHERE id = ?1", &[&id]
+        )?;
+        Ok(())
+    }
 }
